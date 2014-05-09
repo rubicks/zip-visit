a28495f5db8e4abdcf5130314558903de1d834bd
Your Name <you@example.com>
2014-05-09 11:40:37 -0400

    bump version patch

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject zip-visit "1.0.0"
+(defproject zip-visit "1.0.1"
   :description "Clojure zipper-based visitor library."
-  :url "https://github.com/akhudek/zip-visit"
+  :url "https://github.com/rubicks/zip-visit"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :scm {:name "git"
-        :url  "https://github.com/akhudek/zip-visit"})
+        :url  "https://github.com/rubicks/zip-visit"})
